da02c13ceff559e64ff5a1aa00dbc1fe935df549
igatake <t.igai.716@gmail.com>
2019-12-14 23:16:15 +0900

    enable to use fetchAPI

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,8 +1,17 @@
 class MapsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index; end
 
   def fetch_test
-    @shops = JSON.parse(request.body.read)
-    render json: @shops
+    @lat = params[:now_lat]
+    @lng = params[:now_lng]
+    log = Logger.new(STDOUT)
+    log.info(@lat)
+    log.info(@lng)
+    # params[:max_x], params[:min_y], params[:max_y])
+    render json: {
+      lat: @lat,
+      lng: @lng
+    }
   end
 end
