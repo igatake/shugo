39d0c716b37532963ea3405bc129aac6f0554a87
igatake <t.igai.716@gmail.com>
2020-01-04 04:13:21 +0900

    レスポンシブ

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,14 +2,14 @@ class MapsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index; end
 
-  def fetch_test
+  def fetch_shops
     @lat = params[:now_lat]
     @lng = params[:now_lng]
     log = Logger.new(STDOUT)
     log.info(@lat)
     log.info(@lng)
 
-    @shops = Shop.get_shops(@lat, @lng, 10)
+    @shops = Shop.get_shops(@lat, @lng, 20)
     render json: @shops
   end
 end
